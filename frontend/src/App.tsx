458fdf97b149d20770566caa5831eff2fec124c5
Ridwan <awan7w.ra@gmail.com>
2023-08-21 22:23:34 +0700

    Video-V-List-Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-  
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="prouct-image"
+              />
+              <h2>{product.name}</h2>
+              <p>$ {product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   );
